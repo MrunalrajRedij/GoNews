11f9523e91ed02e51710544e56cf5c01c6145dae
MrunalrajRedij <mrunalrajredij@gmail.com>
2024-01-12 12:39:57 +0530

    Update values.dart

--- lib/utils/config/values.dart
+++ lib/utils/config/values.dart
@@ -1,1 +1,9 @@
 const apiKey = "6e16af6c79254a079b1e921faca866ca";
+//https://newsapi.org/
+//0: 6e16af6c79254a079b1e921faca866ca
+//1: d7020af34a2146e4a5e3ea44c64c6dc3
+const String initAmount = '10';
+
+//razorpay
+String api_key = "rzp_test_sj2H4ujnY45Vv1";
+String api_secret = "jYDIRmkDxK53f1huI90kBtZm";
